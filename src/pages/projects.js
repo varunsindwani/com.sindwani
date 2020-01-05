6cb2c5e25eb1ac32f3325275256ed27cb5e7e04a
Varun Sindwani <varun@sindwani.com>
2020-01-04 22:02:54 -0500

    added temp projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,7 @@ import SEO from '../components/seo';
 const ProjectPage = () => (
   <Layout>
     <SEO title="Projects" />
+    <h1 className="text-3xl text-white font-bold ml-auto mr-auto">Coming Soon ... Check out my github in the footer for now!</h1>
   </Layout>
 );
 
